56af1c73640ec26d5c20e4f02facc25531f0f364
briancanty <brian_canty@mba.berkeley.edu>
2015-03-06 10:57:28 -0800

    LionsAndTigers

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Brian Canty on 3/4/15.
+//  Copyright (c) 2015 Brian Canty. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
